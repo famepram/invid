533c0867dc08ed94234bd1a947fd3140e97523ab
femmy_san <femmy.pramana@gmail.com>
2017-10-12 17:06:06 +0700

    mapping frontweb

---
+++ resources/views/bell/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+    <!--[if IE 9 ]><html class="ie9"><![endif]-->
+    <head>
+	@include('bell.inc.load_top') 
+    @yield('meta') 
+    </head>
+
+    <body>
+    	@yield('content') 
+ 	</body>
+</html>
